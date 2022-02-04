88f1e193f36519812c4963ff44d5e7ee4a8c26a2
pluginpatch <pluginpatch@gmail.com>
2022-02-04 15:06:25 -0800

    add check if news data didn't respond correctly

--- index.js
+++ index.js
@@ -34,17 +34,25 @@ let get_news = new cron.CronJob("4,19,34,49 * * * *", async () => {
     response.on("end", () => {
       var article = JSON.parse(data);
       console.log("Latest Sync: " + latest_sync);
-      if(Date.parse(article.data[0].attributes.published_at) >= latest_sync) {
-        console.log("New article found! Published at: " + Date.parse(article.data[0].attributes.published_at))
-        var title = "__**" + article.data[0].attributes.title + "**__\n";
-        var date = "_" + article.data[0].attributes.field_galnet_date + "_\n";
-        var link = "https://community.elitedangerous.com/galnet/uid/" + article.data[0].attributes.field_galnet_guid + "\n";
-        var body = ">>> " + article.data[0].attributes.body.value;
-        body = body.replace(/(\*|_|`|~|\\)/g, '\\$1');
-        var message = title.concat(date, link, body);
-        post(message)
+      // Did we get any data
+      if(article.data && article.data[0]) {
+        // Is the data more recent than the last sync
+        if(Date.parse(article.data[0].attributes.published_at) >= latest_sync) {
+          console.log("New article found! Published at: " + Date.parse(article.data[0].attributes.published_at))
+          var title = "__**" + article.data[0].attributes.title + "**__\n";
+          var date = "_" + article.data[0].attributes.field_galnet_date + "_\n";
+          var link = "https://community.elitedangerous.com/galnet/uid/" + article.data[0].attributes.field_galnet_guid + "\n";
+          var body = ">>> " + article.data[0].attributes.body.value;
+          body = body.replace(/(\*|_|`|~|\\)/g, '\\$1');
+          var message = title.concat(date, link, body);
+          post(message)
+        }
+        // Set the latest sync
+        latest_sync = Date.now()
+      } else {
+        // we received no data, skip this time
+        console.log("Received no data, skipping this run.")
       }
-      latest_sync = Date.now()
     });
   }).on("error", (err) => {
     console.log("Error: " + err.message);
@@ -70,13 +78,13 @@ function post(content) {
 };
 
 // Commands via channelUpdate
-client.on("channelUpdate", function(old_channel, new_channel){
-  if (new_channel.topic && new_channel.topic.includes("galnet-news on")) {
+client.on("channelUpdate", function(old_channel, new_channel) {
+  if(new_channel.topic && new_channel.topic.includes("galnet-news on")) {
     client.channels.cache.get(new_channel.id).send("Galnet News articles will be synced to this channel.\nUpdate the channel topic with `galnet-news off` (or kick the bot) to stop.\nYou can delete this message and remove the channel topic now, if desired.");
     console.log("Adding guild to servers list: " + new_channel.guild.id);
     db.prepare("INSERT OR REPLACE INTO servers (guild_id, channel_id, language) VALUES (?, ?, 'en-GB');").run(new_channel.guild.id, new_channel.id);
   }
-  if (new_channel.topic && new_channel.topic.includes("galnet-news off")) {
+  if(new_channel.topic && new_channel.topic.includes("galnet-news off")) {
     client.channels.cache.get(new_channel.id).send("Galnet News article sync stopped for this channel.\nUpdate the channel topic with `galnet-news on` to resume article sync.\nYou can delete this message and remove the channel topic now, if desired.");
     console.log("Removing guild from servers list: " + new_channel.guild.id);
     db.prepare("DELETE FROM servers WHERE guild_id = ?;").run(new_channel.guild.id);
