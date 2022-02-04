705038a27c73920e6fc0beea85f60cf503a8a3bf
pluginpatch <pluginpatch@gmail.com>
2022-02-04 13:32:45 -0800

    migrate git

---
+++ README.md
@@ -0,0 +1,46 @@
+# Galnet Discord Bot
+Discord bot to post Galnet News from Elite: Dangerous. This bot will check Galnet twice an hour for a new article and post to any subscribed Discord servers.
+
+![Example Image of Bot](/how-to/example_image.jpg?raw=true)
+
+## Add the Bot to your Server
+
+1. [Invite the Bot to your Server](https://discord.com/api/oauth2/authorize?client_id=937466947975651378&permissions=150528&scope=bot)
+2. Give the bot the necessary permissions to post
+3. Edit the channel you want the bot to sync to
+![Editing channel](/how-to/edit_channel.jpg?raw=true)
+4. Add `galnet-news on` to the channel's topic
+![Editing channel topic](/how-to/update_topic.jpg?raw=true)
+5. Save the change
+![Saving changes](/how-to/save_changes.jpg?raw=true)
+6. You can remove the command from the channel's topic now
+
+Done!
+
+## Stop Receiving Posts
+
+1. Edit the channel
+2. Add `galnet-news off` to the channel's topic, save the change
+3. You can remove the command from the channel's topic now
+
+# Host Your Own Bot
+The bot is running on nodejs and discordjs. You can host this on any computer and it's very lightweight.
+
+## Requirements
+* discord.js
+* better-sqlite3
+* cron
+* https
+
+## Setup
+
+1. Pull the repo `git clone https://github.com/jakecsells/galnet-discord-bot.git`
+2. In the repo directory, run `npm update`
+3. Get your discord bot token https://discordapp.com/developers/applications/
+4. Create a file called `token.json` with your token in the repo directory:
+```
+{
+  "token": "<Your Token>"
+}
+```
+5. In the repo directory, run `npm start`
---
+++ index.js
@@ -0,0 +1,92 @@
+// Discord setup
+const { token } = require("./token.json");
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+// Other setup
+const fs = require("fs");
+const https = require("https");
+const cron = require("cron");
+const Database = require('better-sqlite3');
+
+// Create/Connect DB
+const db = new Database('galnet-discord-bot.db', { verbose: console.log });
+db.prepare("CREATE TABLE IF NOT EXISTS servers('guild_id' varchar PRIMARY KEY, 'channel_id' varchar, 'active' bool, 'mention' varchar, 'language' varchar);").run();
+
+// Variables
+var latest_sync = Date.now()
+
+client.on("ready", () => {
+  console.log("I am ready!");
+  get_news.start();
+});
+
+// Check for a new article every 4th, 19th, 34th, and 49th minute
+let get_news = new cron.CronJob("4,19,34,49 * * * *", async () => {
+  https.get("https://cms.zaonce.net/en-GB/jsonapi/node/galnet_article?&sort=-published_at&page[offset]=0&page[limit]=1", (response) => {
+    let data = "";
+    response.setEncoding("utf8");
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+    response.on("end", () => {
+      var article = JSON.parse(data);
+      console.log("Latest Sync: " + latest_sync);
+      if(Date.parse(article.data[0].attributes.published_at) >= latest_sync) {
+        console.log("New article found! Published at: " + Date.parse(article.data[0].attributes.published_at))
+        var title = "__**" + article.data[0].attributes.title + "**__\n";
+        var date = "_" + article.data[0].attributes.field_galnet_date + "_\n";
+        var link = "https://community.elitedangerous.com/galnet/uid/" + article.data[0].attributes.field_galnet_guid + "\n";
+        var body = ">>> " + article.data[0].attributes.body.value;
+        body = body.replace(/(\*|_|`|~|\\)/g, '\\$1');
+        var message = title.concat(date, link, body);
+        post(message)
+      }
+      latest_sync = Date.now()
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+});
+
+// Post to servers
+function post(content) {
+  console.log("Updating all servers with new article.")
+  console.log("New article to post: " + content);
+  var servers = db.prepare("SELECT * FROM servers;").all();
+  servers.forEach((server) => {
+    let channel = client.channels.cache.get(server.channel_id);
+    // If Channel exists, post
+    if(channel) {
+      channel.send(content);
+      console.log("Posting to channel: " + server.channel_id)
+    }
+    else {
+      console.log("Failed getting channel: " + server.channel_id);
+    }
+  });
+};
+
+// Commands via channelUpdate
+client.on("channelUpdate", function(old_channel, new_channel){
+  if (new_channel.topic && new_channel.topic.includes("galnet-news on")) {
+    client.channels.cache.get(new_channel.id).send("Galnet News articles will be synced to this channel.\nUpdate the channel topic with `galnet-news off` (or kick the bot) to stop.\nYou can delete this message and remove the channel topic now, if desired.");
+    console.log("Adding guild to servers list: " + new_channel.guild.id);
+    db.prepare("INSERT OR REPLACE INTO servers (guild_id, channel_id, language) VALUES (?, ?, 'en-GB');").run(new_channel.guild.id, new_channel.id);
+  }
+  if (new_channel.topic && new_channel.topic.includes("galnet-news off")) {
+    client.channels.cache.get(new_channel.id).send("Galnet News article sync stopped for this channel.\nUpdate the channel topic with `galnet-news on` to resume article sync.\nYou can delete this message and remove the channel topic now, if desired.");
+    console.log("Removing guild from servers list: " + new_channel.guild.id);
+    db.prepare("DELETE FROM servers WHERE guild_id = ?;").run(new_channel.guild.id);
+  }
+});
+
+// Remove guild and channel when the bot is removed from the guild.
+client.on("guildDelete", function(guild){
+  console.log("Removing guild from servers list: " + guild.id);
+  db.prepare("DELETE FROM servers WHERE guild_id = ?;").run(guild.id);
+});
+
+client.login(token);
